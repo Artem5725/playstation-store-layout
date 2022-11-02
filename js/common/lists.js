320385cd6306c2a9a92a694b1c623fc7b012cd84
Artem5725 <roadfromreversertowebdesigner@mail.ru>
2022-11-02 20:30:58 +0300

    game page base layout, cards from catalog now redirect to game page; dropdown list buttons now rotate on click

--- js/common/lists.js
+++ js/common/lists.js
@@ -1,13 +1,19 @@
 function show_list(elem) {
     let content = document.getElementById(elem);
     hide_all_lists(content);
-    content.style.display = "block";
-    /* !!! add arguments to callback function via bind
-        1st param (null) will be used as this in function
-        other params - arguments of function
-     */
-    setTimeout(callback_show_list.bind(null, content), 1) // little delay between make content display as block, and animate it
+    if (content.style.display !== "block") {
+        content.style.display = "block";
+        /* !!! add arguments to callback function via bind
+            1st param (null) will be used as this in function
+            other params - arguments of function
+         */
+        setTimeout(callback_show_list.bind(null, content), 1) // little delay between make content display as block, and animate it
 
+        // rotate button
+        rotateButton(this, 180);
+    } else {
+        hide_list(content);
+    }
 }
 
 function callback_show_list(content) {
@@ -21,6 +27,11 @@ function hide_list(content) {
     content.style.top = "200%";
     content.style.opacity = "0";
     content.style.transition = "top 0.5s";
+
+    // rotate button
+    // !!! previousSibling white space is also element
+    let buttonParent = content.previousSibling.previousSibling; // go to face element and then inside it to button
+    rotateButton(buttonParent.getElementsByTagName("button")[0], 0);
 }
 
 function hide_all_lists(except) {
@@ -37,5 +48,9 @@ function hide_all_lists(except) {
     }
 }
 
+function rotateButton(button, deg) {
+    button.style.transform = `rotate(${deg}deg)`;
+    button.style.transitionDuration = "0.5s";
+}
 
 
