c7ca91926b9f9a8a6a1ce839bfe33fae7a031bfc
Artem5725 <roadfromreversertowebdesigner@mail.ru>
2022-11-04 13:38:25 +0300

    game page main img game page feature ine

---
+++ js/pages/game-page-content.js
@@ -0,0 +1,9 @@
+function changeHeart(element) {
+    element.style.color = (element.style.color === "rgb(204, 0, 0)") ? "#302D2D" : " rgb(204, 0, 0)";
+    element.style.borderColor = (element.style.borderColor === "rgb(204, 0, 0)") ? "#302D2D" : " rgb(204, 0, 0)";
+}
+
+function hoverHeart(element){
+    element.style.color = "#FFFFFF";
+    element.style.borderColor = "#FFFFFF";
+}
